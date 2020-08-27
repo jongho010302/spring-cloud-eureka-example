d05c7dcad9f351a5aaa05e3217e6f81ab6a54ca1
jongho <jongho010302@gmail.com>
2020-08-27 09:55:37 +0900

    First Commit

---
+++ src/test/java/net/yogurt/eureka/YogurtEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yogurt.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogurtEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
